5f6a312cf8b44136091b17d59a854cb55e8894f9
Sitong Chen <comeonsitong@gmail.com>
2021-01-23 18:39:55 -0400

    add cesium && resium, display the globe earth.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { hot } from 'react-hot-loader/root';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload Sitongkk.
         </p>
         <a
           className="App-link"
@@ -22,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default hot(App);
