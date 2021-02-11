0ba7b395366c80ea907a0e393a9430ec23c4cafb
Lohins <comeonsitong@gmail.com>
2021-02-10 22:52:28 -0400

    Add ESLint && Prettier (#1)
    * install ESLint and Prettier
    
    * add a script for formatting code
    
    * format the code
    
    * update deploy script

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import HelloWorld from './pages/HelloWorld';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import HelloWorld from "./pages/HelloWorld";
 
 ReactDOM.render(
   <React.StrictMode>
     <HelloWorld />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/HelloWorld.js
+++ src/pages/HelloWorld.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { Viewer, Entity, GeoJsonDataSource } from "resium";
 import { Cartesian3, createWorldTerrain } from "cesium";
-import worldGeoJson from '../assets/geojson/countries-medium.geojson';
+import worldGeoJson from "../assets/geojson/countries-medium.geojson";
 
 const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100);
 const pointGraphics = { pixelSize: 10 };
 const terrainProvider = createWorldTerrain();
 
 const HelloWorld = () => (
-    <Viewer full terrainProvider={terrainProvider}>
-        <Entity 
-            position={position} 
-            point={pointGraphics} 
-            name="Tokyo"
-            description="Hello World"
-        />
+  <Viewer full terrainProvider={terrainProvider}>
+    <Entity
+      position={position}
+      point={pointGraphics}
+      name="Tokyo"
+      description="Hello World"
+    />
 
-        <GeoJsonDataSource data={worldGeoJson}/>
-    </Viewer>
+    <GeoJsonDataSource data={worldGeoJson} />
+  </Viewer>
 );
 
 export default HelloWorld;
